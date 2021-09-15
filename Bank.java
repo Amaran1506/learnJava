5137f1d01cd98ba68502aeba0a05ec005e064eca
Amaran1506 <tnitharshanan@gmail.com>
2021-09-15 11:33:38 +0530

    updatedBank.java

---
+++ Bank.java
@@ -0,0 +1,22 @@
+public class Bank
+{
+public static void main(String[] args)
+{
+Bank cashier = new Bank();
+cashier.deposit();   //method calling statement
+	
+Bank customer = new Bank();
+customer.deposit(500);      //arguments (500)
+customer.deposit(500.5);
+}
+
+void deposit(int cash)
+{
+	System.out.println("depo "+cash);
+}
+void deposit()     //method name: deposit
+{
+	System.out.println("depo");
+}
+}
+
